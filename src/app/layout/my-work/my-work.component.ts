10b560bf8869a3d5ee4ae4ae444e382fd54e6024
Matusz27 <mateusz.wozniak2000@gmail.com>
2021-11-16 18:38:38 +0100

    changed the waz data is stored to json files, edited the discriptions

--- src/app/layout/my-work/my-work.component.ts
+++ src/app/layout/my-work/my-work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import work from "./workshow.json"
 
 @Component({
   selector: 'app-my-work',
@@ -8,29 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class MyWorkComponent implements OnInit {
 
 
-    cards = [
-
-    {imagesource: "./assets/images/eletron.png",type: 4,
-    header:"Elektron",
-    linkLive:"",linkCode:"https://github.com/Matusz27/Elektron",
-    content:"This project is still developing, it is an electronics ecommerce platfrom. I created it to learn Angular + RxJS, Django + REST using postrgress database, currently Its stuck on authentication and authorization, as well as  the cookies holding the cart and sessions. At the moment only the code is alviable to view, as the project has not reached the minimum viable product stage."},
-
-    {imagesource: "./assets/images/wildfriuts.png",type: 3, 
-    header:"Wild Friuts",
-    linkLive:"https://steamcommunity.com/sharedfiles/filedetails/?id=2618566294",linkCode:"",
-    content:"This project is the most sucesful mod for Project Zomboid I made to date, reaching over 5000 subsriptions and over 7000 views and counting, with 50-120 new subscriptions a day, taking #32th place on the most popular mods in past 3 months. I continuely develop it as well as my other mods, adding the suggestion of users to it. It uses Lua language for the code, and json like for defining items."},
-
-    
-    {imagesource: "./assets/images/bot.png",type: 3,
-     header:"Celest bot",
-     linkLive:"",linkCode:"https://github.com/Matusz27/Celest-bot",
-     content:"This project represents all the discord bots I had created. Being the most complex one of them all, using postrgres to store data, and possesing most functions. They are build using python and discord.py library, I had created sevral bots like this in past few years, the most notable one outside of this one would be the School Bot, that was used by my teacher, back at the start of the pandemic to check attendence."},
-    
-    {imagesource: "./assets/images/onxy.png",type: 2,
-     header:"The Onxy Plane",
-     linkLive:"https://matusz27.github.io/Onxy-Plane/Index.html",linkCode:"https://github.com/Matusz27/Onxy-Plane",
-     content:"This is a smaller project, that was build over a project my friend Preston made, I expanded on it, fixed design, and code errors, compeltly redone how the details about the towns are displayed, and added the animations using GreenSocks, turning the barebones page into a intresting expirence."}]
-
+  public cards:{imagesource:string, type:number, header:string, 
+    linkLive:string, linkCode:string, content:string}[] = work;
 
   constructor() { }
 
--- src/app/layout/skills/skills.component.ts
+++ src/app/layout/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import skills from "./skills.json"
 
 @Component({
   selector: 'app-skills',
@@ -7,14 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-
-  cards = [
-    {imagesource: "./assets/vector_images/angular.svg", header:"Frontend Development",
-    content:"I work with Angular to create Frontend applications. I also  learn React, but I put focus on Angular currently. For styling I use SCSS. I know vanilla javascript and typescript, often using Greensocks and ScrollReveal liberies"},
-    {imagesource: "./assets/vector_images/python.svg", header:"Backend Development",
-    content:"For the backend I use Python with Django/Flask libaries. I know how to create REST aplications as well as server rendered web pages.I use PostgresSQL as database of choice and know how to use MySQL"},
-    {imagesource: "./assets/vector_images/Pointer.svg", header:"UI/UX Design",
-    content:"In this area I am least skilled at, but I can create quite compeling, easy to navigate and good looking expirences. Still I need to get some more expirence creating expirences for the users to become proficient with Design "}]
+  public cards:{imagesource:string, header:string, content:string}[] = skills;
 
   constructor() { }
 
